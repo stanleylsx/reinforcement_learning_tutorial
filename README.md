a333262a74114222cd3447ff4b9821c7bac5fc6f
lishouxian <gzlishouxian@gmail.com>
2023-03-13 14:10:43 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# reinforcement_learning_tutorial
-强化学习学习代码
+# 强化学习
+个人强化学习学习代码，学习代码来自于[动手学强化学习](https://hrl.boyuai.com/chapter/1/%E5%88%9D%E6%8E%A2%E5%BC%BA%E5%8C%96%E5%AD%A6%E4%B9%A0)
---
+++ utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class BernoulliBandit:
+    """ 伯努利多臂老虎机,输入K表示拉杆个数 """
+    def __init__(self, K):
+        self.probs = np.random.uniform(size=K)  # 随机生成K个0～1的数,作为拉动每根拉杆的获奖
+        # 概率
+        self.best_idx = np.argmax(self.probs)  # 获奖概率最大的拉杆
+        self.best_prob = self.probs[self.best_idx]  # 最大的获奖概率
+        self.K = K
+
+    def step(self, k):
+        # 当玩家选择了k号拉杆后,根据拉动该老虎机的k号拉杆获得奖励的概率返回1（获奖）或0（未获奖）
+        if np.random.rand() < self.probs[k]:
+            return 1
+        else:
+            return 0
+
+
+def plot_results(solvers, solver_names):
+    """
+    生成累积懊悔随时间变化的图像。输入solvers是一个列表,列表中的每个元素是一种特定的策略。
+    而solver_names也是一个列表,存储每个策略的名称
+    """
+    for idx, solver in enumerate(solvers):
+        time_list = range(len(solver.regrets))
+        plt.plot(time_list, solver.regrets, label=solver_names[idx])
+    plt.xlabel('Time steps')
+    plt.ylabel('Cumulative regrets')
+    plt.title('%d-armed bandit' % solvers[0].bandit.K)
+    plt.legend()
+    plt.show()
